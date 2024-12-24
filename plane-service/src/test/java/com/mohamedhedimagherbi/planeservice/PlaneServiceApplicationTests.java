ac88030910907e69f63e8d5f5ec3850576e9dd34
maruukii <mohamedmagherbi9@gmail.com>
2024-12-24 11:57:09 +0100

    First commit

---
+++ plane-service/src/test/java/com/mohamedhedimagherbi/planeservice/PlaneServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamedhedimagherbi.planeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
